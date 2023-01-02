051d5b0f957b944d695d9dbdae0c950cf78b7674
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2023-01-02 19:43:03 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,75 +1,75 @@
 import * as THREE from 'three';
 
 enum Block {
-  AIR,
-  GRASS,
-  STONE
+    AIR,
+    GRASS,
+    STONE
 }
 
 class SubChunk {
-  y: number
-  isMeshGenerated: boolean
-  voxels: Array<Block>
+    y: number
+    isMeshGenerated: boolean
+    voxels: Array<Block>
 
-  constructor(y: number) {
-    this.y = y
-    this.isMeshGenerated = false
-    this.voxels = new Array(16 * 16 * 16)
-  }
+    constructor(y: number) {
+        this.y = y
+        this.isMeshGenerated = false
+        this.voxels = new Array(16 * 16 * 16)
+    }
 
-  setCubeAtXYZ(x: number, y: number, z: number, v: Block) {
-    if (x < 0 || y < 0 || z < 0 || x >= 16 || y >= 16 || z >= 16) { return }
-    this.voxels[x * 16 * 16 + y * 16 + z] = v
-  }
+    setCubeAtXYZ(x: number, y: number, z: number, v: Block) {
+        if (x < 0 || y < 0 || z < 0 || x >= 16 || y >= 16 || z >= 16) { return }
+        this.voxels[x * 16 * 16 + y * 16 + z] = v
+    }
 
-  getCubeAtXYZ(x: number, y: number, z: number): Block {
-    if (x < 0 || y < 0 || z < 0 || x >= 16 || y >= 16 || z >= 16) { return 0 }
-    return this.voxels[x * 16 * 16 + y * 16 + z]
-  }
+    getCubeAtXYZ(x: number, y: number, z: number): Block {
+        if (x < 0 || y < 0 || z < 0 || x >= 16 || y >= 16 || z >= 16) { return 0 }
+        return this.voxels[x * 16 * 16 + y * 16 + z]
+    }
 
-  getMesh(): THREE.Mesh {
+    getMesh(): THREE.Mesh {
 
-  }
+    }
 }
 
 class Chunk {
-  x: number
-  z: number
-  isTerrainGenerated: boolean
-  subchunks: Array<SubChunk>
+    x: number
+    z: number
+    isTerrainGenerated: boolean
+    subchunks: Array<SubChunk>
 
-  constructor(x: number, z: number) {
-    this.x = x
-    this.z = z
-    this.isTerrainGenerated = false
-    this.subchunks = new Array(16)
-    for (let i: number = 0; i < 16; i++) {
-      this.subchunks[i] = new SubChunk(i)
-    }
-  }
-
-  setBlockAtXYZ(x: number, y: number, z: number, v: Block) {
-    if (x < 0 || y < 0 || z < 0 || x >= 16 || y >= 256 || z >= 16) { return }
-    this.subchunks[Math.floor(y / 16)].setCubeAtXYZ(x, y - 16 * Math.floor(y / 16), z, v)
-  }
-
-  getBlockAtXYZ(x: number, y: number, z: number): Block {
-    if (x < 0 || y < 0 || z < 0 || x >= 16 || y >= 256 || z >= 16) { return Block.AIR }
-    return this.subchunks[Math.floor(y / 16)].getCubeAtXYZ(x, y - 16 * Math.floor(y / 16), z)
-  }
-
-  generateTerrain() {
-    for (let x: number = 0; x < 16; x++) {
-      for (let y: number = 0; y < 256; y++) {
-        for (let z: number = 0; z < 16; z++) {
-          this.setBlockAtXYZ(x, y, z, y < 60 ? Block.GRASS : Block.AIR)
+    constructor(x: number, z: number) {
+        this.x = x
+        this.z = z
+        this.isTerrainGenerated = false
+        this.subchunks = new Array(16)
+        for (let i: number = 0; i < 16; i++) {
+            this.subchunks[i] = new SubChunk(i)
         }
-      }
     }
-    this.isTerrainGenerated = true
-  }
 
-  generateMesh() {
-    if (!this.isTerrainGenerated) { return }
-  }
+    setBlockAtXYZ(x: number, y: number, z: number, v: Block) {
+        if (x < 0 || y < 0 || z < 0 || x >= 16 || y >= 256 || z >= 16) { return }
+        this.subchunks[Math.floor(y / 16)].setCubeAtXYZ(x, y - 16 * Math.floor(y / 16), z, v)
+    }
+
+    getBlockAtXYZ(x: number, y: number, z: number): Block {
+        if (x < 0 || y < 0 || z < 0 || x >= 16 || y >= 256 || z >= 16) { return Block.AIR }
+        return this.subchunks[Math.floor(y / 16)].getCubeAtXYZ(x, y - 16 * Math.floor(y / 16), z)
+    }
+
+    generateTerrain() {
+        for (let x: number = 0; x < 16; x++) {
+            for (let y: number = 0; y < 256; y++) {
+                for (let z: number = 0; z < 16; z++) {
+                    this.setBlockAtXYZ(x, y, z, y < 60 ? Block.GRASS : Block.AIR)
+                }
+            }
+        }
+        this.isTerrainGenerated = true
+    }
+
+    generateMesh() {
+        if (!this.isTerrainGenerated) { return }
+    }
 }
