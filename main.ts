8af83b607c53513eff9fa0c3138d49d12e365178
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2023-01-03 19:32:05 +0100

    Moved main.ts

--- main.ts
+++ main.ts
@@ -13,12 +13,14 @@ class SubChunk {
     isMeshGenerated: boolean
     voxels: Array<Block>
     mesh: THREE.Mesh | null
+    chunk: Chunk
 
-    constructor(y: number) {
+    constructor(y: number, c: Chunk) {
         this.y = y
         this.isMeshGenerated = false
         this.voxels = new Array(16 * 16 * 16)
         this.mesh = null
+        this.chunk = c
     }
 
     setCubeAtXYZ(x: number, y: number, z: number, v: Block) {
@@ -31,7 +33,7 @@ class SubChunk {
         return this.voxels[x * 16 * 16 + y * 16 + z]
     }
 
-    generateMesh(cx: number, cz: number) {
+    generateMesh() {
         let pxGeometry = new THREE.PlaneGeometry(100, 100);
         pxGeometry.attributes.uv.array[1] = 0.5;
         pxGeometry.attributes.uv.array[3] = 0.5;
@@ -41,14 +43,20 @@ class SubChunk {
         let nxGeometry = new THREE.PlaneGeometry(100, 100);
         nxGeometry.attributes.uv.array[1] = 0.5;
         nxGeometry.attributes.uv.array[3] = 0.5;
-        nxGeometry.rotateY(- Math.PI / 2);
-        nxGeometry.translate(- 50, 0, 0);
+        nxGeometry.rotateY(-Math.PI / 2);
+        nxGeometry.translate(-50, 0, 0);
 
         let pyGeometry = new THREE.PlaneGeometry(100, 100);
         pyGeometry.attributes.uv.array[5] = 0.5;
         pyGeometry.attributes.uv.array[7] = 0.5;
-        pyGeometry.rotateX(- Math.PI / 2);
+        pyGeometry.rotateX(-Math.PI / 2);
         pyGeometry.translate(0, 50, 0);
+
+        let nyGeometry = new THREE.PlaneGeometry(100, 100);
+        nyGeometry.attributes.uv.array[5] = 0.5;
+        nyGeometry.attributes.uv.array[7] = 0.5;
+        nyGeometry.rotateX(Math.PI / 2);
+        nyGeometry.translate(0, -50, 0);
 
         let pzGeometry = new THREE.PlaneGeometry(100, 100);
         pzGeometry.attributes.uv.array[1] = 0.5;
@@ -59,41 +67,29 @@ class SubChunk {
         nzGeometry.attributes.uv.array[1] = 0.5;
         nzGeometry.attributes.uv.array[3] = 0.5;
         nzGeometry.rotateY(Math.PI);
-        nzGeometry.translate(0, 0, - 50);
+        nzGeometry.translate(0, 0, -50);
 
         const geometries = [];
-        for (let z = 0; z < 16; z++) {
 
-            for (let x = 0; x < 16; x++) {
-                const h = 60;
+        for (let x: number = 0; x < 16; x++) {
+            for (let y: number = 0; y < 16; y++) {
+                for (let z: number = 0; z < 16; z++) {
+                    matrix.makeTranslation(
+                        x * 100 + this.chunk.x * 100 * 16,
+                        y * 100 + this.y * 100 * 16,
+                        z * 100 + this.chunk.z * 100 * 16
+                    );
 
-                matrix.makeTranslation(
-                    x * 100 * cx * 100 * 16,
-                    h * 100,
-                    z * 100 * cz * 100 * 16
-                );
+                    let block = this.chunk.getBlockAtXYZ(x, y + 16 * this.y, z)
 
-                const px = 60;
-                const nx = 60;
-                const pz = 60;
-                const nz = 60;
-
-                geometries.push(pyGeometry.clone().applyMatrix4(matrix));
-
-                if ((px !== h && px !== h + 1) || x === 0) {
-                    geometries.push(pxGeometry.clone().applyMatrix4(matrix));
-                }
-
-                if ((nx !== h && nx !== h + 1) || x === 16 - 1) {
-                    geometries.push(nxGeometry.clone().applyMatrix4(matrix));
-                }
-
-                if ((pz !== h && pz !== h + 1) || z === 16 - 1) {
-                    geometries.push(pzGeometry.clone().applyMatrix4(matrix));
-                }
-
-                if ((nz !== h && nz !== h + 1) || z === 0) {
-                    geometries.push(nzGeometry.clone().applyMatrix4(matrix));
+                    if (block == Block.GRASS) {
+                        geometries.push(pxGeometry.clone().applyMatrix4(matrix));
+                        geometries.push(nxGeometry.clone().applyMatrix4(matrix));
+                        geometries.push(pyGeometry.clone().applyMatrix4(matrix));
+                        geometries.push(nyGeometry.clone().applyMatrix4(matrix));
+                        geometries.push(pzGeometry.clone().applyMatrix4(matrix));
+                        geometries.push(nzGeometry.clone().applyMatrix4(matrix));
+                    }
                 }
             }
         }
@@ -121,7 +117,7 @@ class Chunk {
         this.isTerrainGenerated = false
         this.subchunks = new Array(16)
         for (let i: number = 0; i < 16; i++) {
-            this.subchunks[i] = new SubChunk(i)
+            this.subchunks[i] = new SubChunk(i, this)
         }
     }
 
@@ -139,7 +135,8 @@ class Chunk {
         for (let x: number = 0; x < 16; x++) {
             for (let y: number = 0; y < 256; y++) {
                 for (let z: number = 0; z < 16; z++) {
-                    this.setBlockAtXYZ(x, y, z, y < 60 ? Block.GRASS : Block.AIR)
+
+                    this.setBlockAtXYZ(x, y, z, Math.random() < 0.6 ? Block.GRASS : Block.AIR)
                 }
             }
         }
@@ -150,7 +147,7 @@ class Chunk {
         if (!this.isTerrainGenerated) { return }
         for (let scn = 0; scn < this.subchunks.length; scn++) {
             let sc = this.subchunks[scn]
-            sc.generateMesh(this.x, this.z)
+            sc.generateMesh()
             scene.add(sc.getMesh())
         }
     }
@@ -178,7 +175,7 @@ function init() {
 
     matrix = new THREE.Matrix4();
 
-    atlas = new THREE.TextureLoader().load('../public/atlas.png');
+    atlas = new THREE.TextureLoader().load('public/atlas.png');
     atlas.magFilter = THREE.NearestFilter;
 
     const ambientLight = new THREE.AmbientLight(0xcccccc);
@@ -188,10 +185,12 @@ function init() {
     directionalLight.position.set(1, 1, 0.5).normalize();
     scene.add(directionalLight);
 
-    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        canvas: document.querySelector("#container")
+    });
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
-    container.appendChild(renderer.domElement);
 
     controls = new FirstPersonControls(camera, renderer.domElement);
     controls.movementSpeed = 1000;
@@ -204,8 +203,10 @@ function init() {
     mainChunk.generateTerrain()
     mainChunk.generateMesh()
 
-    const axesHelper = new THREE.AxesHelper( 5 );
-    scene.add( axesHelper );
+    const axesHelper = new THREE.AxesHelper(5);
+    scene.add(axesHelper);
+
+    animate();
 }
 
 function onWindowResize() {
@@ -224,3 +225,5 @@ function render() {
     controls.update(clock.getDelta());
     renderer.render(scene, camera)
 }
+
+init()
