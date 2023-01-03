d1bc9a155787eeb7216138e8749d7e6bbf52c0db
FabulousFox <78906517+FabulousCodingFox@users.noreply.github.com>
2023-01-03 19:00:54 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import { FirstPersonControls } from 'three/examples/jsm/controls/FirstPersonControls';
+import * as BufferGeometryUtils from 'three/examples/jsm/utils/BufferGeometryUtils';
 
 enum Block {
     AIR,
@@ -10,11 +12,13 @@ class SubChunk {
     y: number
     isMeshGenerated: boolean
     voxels: Array<Block>
+    mesh: THREE.Mesh | null
 
     constructor(y: number) {
         this.y = y
         this.isMeshGenerated = false
         this.voxels = new Array(16 * 16 * 16)
+        this.mesh = null
     }
 
     setCubeAtXYZ(x: number, y: number, z: number, v: Block) {
@@ -27,12 +31,81 @@ class SubChunk {
         return this.voxels[x * 16 * 16 + y * 16 + z]
     }
 
-    generateMesh(){
+    generateMesh(cx: number, cz: number) {
+        let pxGeometry = new THREE.PlaneGeometry(100, 100);
+        pxGeometry.attributes.uv.array[1] = 0.5;
+        pxGeometry.attributes.uv.array[3] = 0.5;
+        pxGeometry.rotateY(Math.PI / 2);
+        pxGeometry.translate(50, 0, 0);
 
+        let nxGeometry = new THREE.PlaneGeometry(100, 100);
+        nxGeometry.attributes.uv.array[1] = 0.5;
+        nxGeometry.attributes.uv.array[3] = 0.5;
+        nxGeometry.rotateY(- Math.PI / 2);
+        nxGeometry.translate(- 50, 0, 0);
+
+        let pyGeometry = new THREE.PlaneGeometry(100, 100);
+        pyGeometry.attributes.uv.array[5] = 0.5;
+        pyGeometry.attributes.uv.array[7] = 0.5;
+        pyGeometry.rotateX(- Math.PI / 2);
+        pyGeometry.translate(0, 50, 0);
+
+        let pzGeometry = new THREE.PlaneGeometry(100, 100);
+        pzGeometry.attributes.uv.array[1] = 0.5;
+        pzGeometry.attributes.uv.array[3] = 0.5;
+        pzGeometry.translate(0, 0, 50);
+
+        let nzGeometry = new THREE.PlaneGeometry(100, 100);
+        nzGeometry.attributes.uv.array[1] = 0.5;
+        nzGeometry.attributes.uv.array[3] = 0.5;
+        nzGeometry.rotateY(Math.PI);
+        nzGeometry.translate(0, 0, - 50);
+
+        const geometries = [];
+        for (let z = 0; z < 16; z++) {
+
+            for (let x = 0; x < 16; x++) {
+                const h = 60;
+
+                matrix.makeTranslation(
+                    x * 100 * cx * 100 * 16,
+                    h * 100,
+                    z * 100 * cz * 100 * 16
+                );
+
+                const px = 60;
+                const nx = 60;
+                const pz = 60;
+                const nz = 60;
+
+                geometries.push(pyGeometry.clone().applyMatrix4(matrix));
+
+                if ((px !== h && px !== h + 1) || x === 0) {
+                    geometries.push(pxGeometry.clone().applyMatrix4(matrix));
+                }
+
+                if ((nx !== h && nx !== h + 1) || x === 16 - 1) {
+                    geometries.push(nxGeometry.clone().applyMatrix4(matrix));
+                }
+
+                if ((pz !== h && pz !== h + 1) || z === 16 - 1) {
+                    geometries.push(pzGeometry.clone().applyMatrix4(matrix));
+                }
+
+                if ((nz !== h && nz !== h + 1) || z === 0) {
+                    geometries.push(nzGeometry.clone().applyMatrix4(matrix));
+                }
+            }
+        }
+
+        const geometry = BufferGeometryUtils.mergeBufferGeometries(geometries);
+        geometry.computeBoundingSphere();
+
+        this.mesh = new THREE.Mesh(geometry, new THREE.MeshLambertMaterial({ map: atlas, side: THREE.DoubleSide }));
     }
 
-    getMesh(): THREE.Mesh {
-
+    getMesh(): THREE.Mesh | null {
+        return this.mesh
     }
 }
 
@@ -75,20 +148,76 @@ class Chunk {
 
     generateMesh() {
         if (!this.isTerrainGenerated) { return }
-        for(let sc in this.subchunks ){
-            sc: SubChunk
-            sc.generateMesh()
-            sc.getMesh()
+        for (let scn = 0; scn < this.subchunks.length; scn++) {
+            let sc = this.subchunks[scn]
+            sc.generateMesh(this.x, this.z)
+            scene.add(sc.getMesh())
         }
     }
 }
 
-let camera: THREE.PerspectiveCamera, scene: THREE.Scene;
+let camera: THREE.PerspectiveCamera
+let scene: THREE.Scene;
+let clock: THREE.Clock;
+let controls: FirstPersonControls;
+let renderer: THREE.WebGLRenderer;
+let matrix: THREE.Matrix4;
+let atlas: THREE.Texture;
+let container: HTMLElement | null;
 
-function init(){
-    camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 20000 );
+function init() {
+    container = document.getElementById("container")
+
+    clock = new THREE.Clock();
+
+    camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 20000);
     camera.position.y = 100
 
     scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0xbfd1e5 );
+    scene.background = new THREE.Color(0xbfd1e5);
+
+    matrix = new THREE.Matrix4();
+
+    atlas = new THREE.TextureLoader().load('textures/minecraft/atlas.png');
+    atlas.magFilter = THREE.NearestFilter;
+
+    const ambientLight = new THREE.AmbientLight(0xcccccc);
+    scene.add(ambientLight);
+
+    const directionalLight = new THREE.DirectionalLight(0xffffff, 2);
+    directionalLight.position.set(1, 1, 0.5).normalize();
+    scene.add(directionalLight);
+
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    controls = new FirstPersonControls(camera, renderer.domElement);
+    controls.movementSpeed = 1000;
+    controls.lookSpeed = 0.125;
+    controls.lookVertical = true;
+
+    window.addEventListener('resize', onWindowResize);
+
+    let mainChunk = new Chunk(0, 0);
+    mainChunk.generateTerrain()
+    mainChunk.generateMesh()
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    controls.handleResize();
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+}
+
+function render() {
+    controls.update(clock.getDelta());
+    renderer.render(scene, camera)
 }
